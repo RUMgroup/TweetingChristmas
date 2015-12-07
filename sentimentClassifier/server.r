f1dcee137f0ccdbb2e326025f4ea3c613e653569
Marco Smolla <marco.smolla@postgrad.manchester.ac.uk>
2015-12-07 19:49:50 +0000

    Added alternative  tweet selection method, not functional yet
    I thought it might be easier to retrieve the ids for the tweets to
    categorise once at the beginning and then just go through them,
    however, with the commented out lines uncommented … it won’t even start
    … so we are still stuck with a shiny app that is slow and periodically
    crashing

--- sentimentClassifier/server.r
+++ sentimentClassifier/server.r
@@ -1,12 +1,13 @@
 library(shiny)
 
 load("tweets.Christmas.sampled.RData")
-tmp <- tmp
+# tmp <- tweets.Christmas.sampled
 if(!any(names(tmp)%in%c('sentiment'))){
 	tmp$sentiment <- NA
 	tmp$id <- 1:nrow(tmp)
-	}
-
+}
+# counter <- 1
+# ids <- tmp[ is.na(tmp[,'sentiment']) ,'id']
 updateID <- function(tmp){
 	sample(tmp[ is.na(tmp[,'sentiment']) ,'id'], size=1)
 }
@@ -14,9 +15,10 @@ updateID <- function(tmp){
 shinyServer(function(input, output) {
 	
 	values <- reactiveValues(id=NA)
+	# values[['id']] <- ids[counter]
 
 	# observeEvent(input$start, {
-	values[['id']] <- updateID(tmp)
+		values[['id']] <- updateID(tmp)
 	# })
 	
 		
@@ -24,25 +26,33 @@ shinyServer(function(input, output) {
 		tmp[ tmp[,'id']==values[['id']] ,'sentiment'] <<- 4
 		save(tmp, file = "tweets.Christmas.sampled.RData")
 		values[['id']] <<- updateID(tmp)
+		# counter <<- counter+1
+		# values[['id']] <<- ids[counter]
 	})
 	observeEvent(input$submitNEU, {
 		tmp[ tmp[,'id']==values[['id']] ,'sentiment'] <<- 2
 		save(tmp, file = "tweets.Christmas.sampled.RData")
 		values[['id']] <<- updateID(tmp)
+		# counter <<- counter+1
+		# values[['id']] <<- ids[counter]
 	})
 	observeEvent(input$submitNEG, {
 		tmp[ tmp[,'id']==values[['id']] ,'sentiment'] <<- 0
 		save(tmp, file = "tweets.Christmas.sampled.RData")
 		values[['id']] <<- updateID(tmp)
+		# counter <<- counter+1
+		# values[['id']] <<- ids[counter]
 	})
 	
-	observeEvent(input$save, {
-		save(tmp, file = "tweets.Christmas.sampled.RData")
-		# stop()
-	})
+# 	observeEvent(input$save, {
+# 		save(tmp, file = "tweets.Christmas.sampled.RData")
+# 		# stop()
+# 	})
 	
+	# Displays the current tweet
 	output$tweet <- renderText({ 
-		ifelse(is.na(values[['id']]), 'Press start', as.character(tmp[ values[['id']] ,'text']))
+		as.character(tmp[ values[['id']] ,'text'])
+		# ifelse(is.na(values[['id']]), 'Press start', as.character(tmp[ values[['id']] ,'text']))
 	})
 	
 })
