65509c632a04caf4c79b779d2756dfe89f56c1c8
Marco Smolla <marco.smolla@postgrad.manchester.ac.uk>
2015-12-07 19:47:57 +0000

    cleaning up comments in UI.R file

--- sentimentClassifier/ui.r
+++ sentimentClassifier/ui.r
@@ -1,25 +1,21 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 	
-	# Application title
 	titlePanel("Sentimenting tweets"),
 	
-	# Sidebar with a slider input for the number of bins
 	sidebarLayout(
 		sidebarPanel(
 			textOutput("tweet")
 		),
 		
-		# Show a plot of the generated distribution
 		mainPanel(
 			# actionButton("start", "Start", class = "btn-primary"),
 			actionButton("submitPOS", "Positive", class = "btn-primary"),
 			actionButton("submitNEU", "Neutral", class = "btn-primary"),
 			actionButton("submitNEG", "Negative", class = "btn-primary"),
-			p("Classify the tweet to display a new tweet."),
-			actionButton("save", "Finish and save", class = "btn-primary")
+			p("Classify the tweet to display a new tweet.")#,
+			# actionButton("save", "Finish and save", class = "btn-primary")
 		)
 	)
 ))
